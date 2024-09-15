d1b73336a4caffaf17ad0cea0c4845001f961d6a
dradvm <duynnvn2511@gmail.com>
2024-09-15 23:54:28 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+const contactsRouter = require("./app/routes/contact.route")
 const express = require("express");
 const cors = require("cors");
 
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/contacts", contactsRouter)
 app.get("/", (req, res) => {
     res.json({ message: "Welcom to contact book application." });
 });
